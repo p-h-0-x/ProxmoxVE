b3b05eeebc4fd7444f3d0b6329faa68159185f07
Ph0xel <merieaub@gmail.com>
2025-08-26 14:46:03 +0200

    testing

--- ct/tube-archivist.sh
+++ ct/tube-archivist.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/p-h-0-x/ProxmoxVE/refs/heads/feat-tube-archivist/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: benjaminmerieau
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
